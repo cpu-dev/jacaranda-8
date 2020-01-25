f7d1423d36c7a16f65eae83652ec569d52e582d8
cra2ypierr0t <uchiyama.kazuhide@gmail.com>
2020-01-25 17:55:35 +0900

    add uart_flag1

--- src/charlatan/computer.v
+++ src/charlatan/computer.v
@@ -9,14 +9,14 @@ module computer(
     wire [7:0] rs_data;
     wire mem_w_en;
     wire [7:0] mem_r_data;
-    reg [7:0] _mem_r_data;
-    wire tx_en, rx_en;
+    wire [7:0] _mem_r_data;
+    reg tx_en, rx_en;
     reg begin_flag;
     reg [7:0] tx_data;
     wire [7:0] rx_data;
 
     reg [7:0] uart_flag;
-    reg [7:0] uart_flag2;
+    wire [7:0] uart_flag2;
 
 
     instr_mem instr_mem(.addr(pc),
@@ -29,6 +29,20 @@ module computer(
             .rs_data(rs_data),
             .mem_w_en(mem_w_en),
             .mem_r_data(mem_r_data));
+
+    always @(posedge clock) begin
+        if(rs_data == 8'd255 && mem_w_en == 1) begin
+            {rx_en, tx_en} <= rd_data[1:0];
+        end
+    end
+
+    always @(posedge clock) begin
+        if(rs_data == 8'd253 && mem_w_en == 1) begin
+            tx_data <= rd_data;
+        end else begin
+            tx_data <= tx_data;
+        end
+    end
 
     data_mem data_mem(.addr(rs_data),
                       .w_data(rd_data),
