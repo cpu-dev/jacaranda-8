e4e4239e945d938db604779eef9f430c77d55950
Heppokoyuki <yuki@uefi.jp>
2020-01-25 17:44:04 +0900

    add uart_flag2

--- src/charlatan/computer.v
+++ src/charlatan/computer.v
@@ -9,14 +9,15 @@ module computer(
     wire [7:0] rs_data;
     wire mem_w_en;
     wire [7:0] mem_r_data;
+    reg [7:0] _mem_r_data;
     wire tx_en, rx_en;
     reg begin_flag;
-    reg busy_flag;
-    wire receive_flag;
     reg [7:0] tx_data;
     wire [7:0] rx_data;
 
     reg [7:0] uart_flag;
+    reg [7:0] uart_flag2;
+
 
     instr_mem instr_mem(.addr(pc),
                         .instr(instr));
@@ -32,8 +33,10 @@ module computer(
     data_mem data_mem(.addr(rs_data),
                       .w_data(rd_data),
                       .w_en(mem_w_en),
-                      .r_data(mem_r_data),
+                      .r_data(_mem_r_data),
                       .clock(clock));
+
+    assign mem_r_data = (rs_data == 8'd254) ? uart_flag2 : _mem_r_data;
     
     UART UART(.clk(clock),
               .tx_en(tx_en),
@@ -43,7 +46,7 @@ module computer(
               .tx_data(tx_data),
               .tx(tx),
               .rx_data(rx_data),
-              .busy_flag(busy_flag),
-              .receive_flag(receive_flag));
+              .busy_flag(uart_flag2[0]),
+              .receive_flag(uart_flag2[1]));
 
 endmodule
