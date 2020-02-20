ddc20828c97bd0488f0b9b91520b69275e6741cf
cra2ypierr0t <uchiyama.kazuhide@gmail.com>
2020-02-21 00:46:39 +0900

    interrupt

--- src/charlatan/computer.v
+++ src/charlatan/computer.v
@@ -16,6 +16,16 @@ module computer(
     reg [7:0] tx_data;
     wire [7:0] rx_data;
 
+    reg [7:0] _ret_addr;
+    reg [7:0] int_vec;
+    reg [7:0] int_en;
+
+    wire int_req;
+
+    wire reg_w_en;
+
+    wire [7:0] ret_addr;
+
     instr_mem instr_mem(.addr(pc),
                         .instr(instr));
 
@@ -25,7 +35,12 @@ module computer(
             .rd_data(rd_data),
             .rs_data(rs_data),
             .mem_w_en(mem_w_en),
-            .mem_r_data(mem_r_data));
+            .mem_r_data(mem_r_data),
+            .int_req(int_req),
+            .int_en(int_en),
+            .int_vec(int_vec),
+            .reg_w_en(reg_w_en),
+            .ret_addr(ret_addr));
 
     always @(posedge clock) begin
         if(rs_data == 8'd255 && mem_w_en == 1) begin
@@ -49,9 +64,32 @@ module computer(
                       .r_data(_mem_r_data),
                       .clock(clock));
 
-    assign mem_r_data = (rs_data == 8'd254) 
-        ? {7'b0, busy_flag}
-        : _mem_r_data;
+    assign mem_r_data = (rs_data == 8'd254) ? {7'b0, busy_flag}
+                      : (rs_data == 8'd252) ? rx_data   //UART RX
+                      : (rs_data == 8'd251) ? _ret_addr  //戻りアドレス
+                      : (rs_data == 8'd250) ? int_vec   //割り込みベクタ
+                      : _mem_r_data;
+
+
+    //割り込み要求が立っている時は割り込み不許可
+    always @(posedge clock) begin
+        if(int_req == 1'b1) begin
+            int_en <= 8'h00;
+            _ret_addr <= ret_addr;
+        end else if(int_req == 1'b0) begin
+            int_en <= 8'h01;
+            _ret_addr <= _ret_addr;
+        end
+    end
+
+    always @(posedge clock) begin
+        //割り込みベクタの書き込み
+        if(rs_data == 8'd250 && mem_w_en == 1'b1) begin
+            int_vec <= rd_data;
+        end else begin
+            int_vec <= int_vec;
+        end
+    end
 
     UART UART(.clk(clock),
               .tx_en(tx_en),
@@ -62,6 +100,9 @@ module computer(
               .tx(tx),
               .rx_data(rx_data),
               .busy_flag(busy_flag),
-              .receive_flag(receive_flag));
+              .receive_flag(receive_flag),
+              .int_req(int_req),
+              .access_addr(rs_data),
+              .reg_w_en(reg_w_en));
 
 endmodule
