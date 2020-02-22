9e5dea07d11385cf4da0bf167cb99dc1b3089cb6
cra2ypierr0t <uchiyama.kazuhide@gmail.com>
2020-02-23 03:52:32 +0900

    modified:   LED4.v

--- src/charlatan/LED/LED4.v
+++ src/charlatan/LED/LED4.v
@@ -4,24 +4,24 @@ module LED4(input [7:0] in_data,
             output reg [3:0] out_data,
             input clock);
     
-    reg [19:0] delay_cnt = 20'h00000;
+    reg [27:0] delay_cnt = 28'h0000000;
     reg [7:0] buffer;
 
     always @(posedge clock) begin
-        if(delay_cnt == 20'h00000 && begin_flag == 1'b0) begin
-            delay_cnt <= 20'h00000;
+        if(delay_cnt == 28'h0000000 && begin_flag == 1'b0) begin
+            delay_cnt <= 28'h0000000;
             state_reg <= 8'b00000000;
             buffer <= in_data;
-        end else if(delay_cnt == 20'h00000 && begin_flag == 1'b1) begin
-            delay_cnt <= delay_cnt + 20'h00001;
+        end else if(delay_cnt == 28'h0000000 && begin_flag == 1'b1) begin
+            delay_cnt <= delay_cnt + 28'h0000001;
             state_reg <= 8'b00000001;
             buffer <= in_data;
-        end else if(delay_cnt != 20'h7a120) begin
-            delay_cnt <= delay_cnt + 20'h00001;
+        end else if(delay_cnt != 28'h2faf080) begin
+            delay_cnt <= delay_cnt + 28'h0000001;
             state_reg <= 8'b00000001;
             buffer <= buffer;
-        end else if(delay_cnt == 20'h7a120) begin
-            delay_cnt <= 20'h00000;
+        end else if(delay_cnt == 28'h2faf080) begin
+            delay_cnt <= 28'h0000000;
             state_reg <= 8'b00000001;
             out_data <= buffer[3:0];
         end
