e6e9ce03fca2d8093a3116df32bcf0ab1b0c5d44
cra2ypierr0t <uchiyama.kazuhide@gmail.com>
2020-02-22 20:56:39 +0900

    Changes to be committed: 	modified:   test_bench.v

--- src/charlatan/test_bench.v
+++ src/charlatan/test_bench.v
@@ -1,24 +1,46 @@
+`timescale 1ns/1ps
 module test_bench();
-    reg clock;
-    wire rx, tx;
+parameter TIMESCALE_HZ = 1000_000_000;
+parameter CLOCK_HZ = 50_000_000;
+parameter BAUD_RATE = 115200;
+parameter TIME_CLOCK = $floor(TIMESCALE_HZ / CLOCK_HZ);
+parameter TIME_BIT = $floor(TIMESCALE_HZ / BAUD_RATE);
+    reg clk;
+    reg rx;
+    wire[7:0] data;
+    wire end_flag;
+    computer cmptr(clk, rx, tx);
 
-    initial begin
-        $dumpfile("wave.vcd");
-        $dumpvars(0, computer);
-    end
+always #(TIME_CLOCK/2) clk = ~clk;
 
-    initial begin
-        clock = 1'b0;
-    end
-
-    computer computer(clock, rx, tx);
-
-    always #1 begin
-        clock = ~clock;
-    end
-
-    initial begin
-        #100000
-        $finish;
-    end
+initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0, cmptr);
+    clk = 1'b0;
+    #1000
+    rx = 1'b1;
+    #(2*TIME_BIT);
+    rx = 1'b0;
+    #(TIME_BIT);
+    rx = 1'b0;
+    #(TIME_BIT);
+    rx = 1'b1;
+    #(TIME_BIT);
+    rx = 1'b0;
+    #(TIME_BIT);
+    rx = 1'b1;
+    #(TIME_BIT);
+    rx = 1'b0;
+    #(TIME_BIT);
+    rx = 1'b1;
+    #(TIME_BIT);
+    rx = 1'b0;
+    #(TIME_BIT);
+    rx = 1'b1;
+    #(TIME_BIT);
+    rx= 1'b1;
+    #(2*TIME_BIT);
+    #10000
+    $finish;
+end
 endmodule
