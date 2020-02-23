69f892287efed733613b65743ae0900235cf629c
Heppokoyuki <yuki@uefi.jp>
2020-02-23 13:47:59 +0900

    kick kuso bug

--- src/charlatan/7SEG/bin_dec_decoder.v
+++ src/charlatan/7SEG/bin_dec_decoder.v
@@ -4,9 +4,9 @@ module bin_dec_decoder(bin_in, dig_3, dig_2, dig_1);
     output [3:0] dig_2;
     output [3:0] dig_1;
 
-    assign dig_1 = (bin_in/10) % 10;
-    assign dig_2 = (bin_in/100) % 10;
-    assign dig_3 = (bin_in/1000) % 10;
+    assign dig_1 = bin_in % 10;
+    assign dig_2 = (bin_in/10) % 10;
+    assign dig_3 = (bin_in/100) % 10;
 
 endmodule
 
